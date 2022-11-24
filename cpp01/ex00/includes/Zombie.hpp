c22ac769f4fdd116283195cd276ee11d974d5107
VictorDé <vducoulo@student.42lyon.fr>
2022-11-24 10:47:52 +0100

    42

---
+++ cpp01/ex00/includes/Zombie.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vducoulo <vducoulo@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 01:35:09 by vducoulo          #+#    #+#             */
+/*   Updated: 2022/11/24 01:36:26 by vducoulo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef CPP_ZOMBIE_H
+#define CPP_ZOMBIE_H
+#include <iostream>
+
+class Zombie 
+{
+private :
+    std::string _name;
+public :
+    void    announce(void);
+};
+
+#endif
